e845443730e1fd2dddc07f0344715b5e6cac3947
Landry Soules <landry.soules@gmail.com>
2015-06-04 19:23:38 +0200

    Added missing scenarios for snippet feature:  - update  - delete  - create  - show

--- features/snippet.feature
+++ features/snippet.feature
@@ -4,5 +4,32 @@ Feature: Snippet life cycle
     Given I am authenticated
     When I go to the new snippet page
     And I fill in the snippet creation form
-    Then I should see a success message
+    Then I should see a "success" message
     And I should see my snippets page
+
+  Scenario: Updating a snippet
+    Given I am authenticated
+    When I go to my snippets page
+    And I click edit on a snippet
+    And I fill in the snippet edit form
+    Then I should see a "update success" message
+    And I should see my snippet page
+
+  @javascript
+  Scenario: Deleting a snippet
+    Given I am authenticated
+    When I go to my snippets page
+    And I click delete on a snippet
+    And I click ok on the confirm box
+    Then I should see a "update delete" message
+    And I should see snippets page
+
+  Scenario: Displaying a snippet
+    Given I am on the home page
+    When I click show on a snippet
+    Then I should see this snippet details
+
+  Scenario: Displaying my snippets
+    Given I am authenticated
+    When I go to my snippets page
+    Then I should see only my snippets
--- features/step_definitions/snippet_steps.rb
+++ features/step_definitions/snippet_steps.rb
@@ -1,5 +1,6 @@
 Given(/^I am authenticated$/) do
-  user = User.new email: "landry.soules@gmail.com", password: "1Password", username: "landry"
+  # FIXME: is it possible to use factorygirl here (build(:user)) in place of User.new ?
+  user = User.new email: "landry.soules@gmail.com", password: "7ninahartley7", username: "landry"
   signup user
   authenticate user
   expect(page).to have_content("Signed in successfully.")
@@ -15,10 +16,68 @@ When(/^I fill in the snippet creation form$/) do
   click_button "submit_snipet"
 end
 
-Then(/^I should see a success message$/) do
-  expect(page).to have_content("Snipet was successfully created.")
+Then(/^I should see a "(.*?)" message$/) do |message_type|
+  if message_type.equal? "success"
+    expect(page).to have_content("Snipet was successfully created.")
+  elsif message_type.equal? "update success"
+    expect(page).to have_content("Snipet was successfully updated.")
+  elsif message_type.equal? "update delete"
+    expect(page).to have_content("Snipet was successfully destroyed.")
+  end
 end
 
 Then(/^I should see my snippets page$/) do
   expect(current_path).to match(/users\/\d+\/snipets/)
+end
+
+When(/^I go to my snippets page$/) do
+  visit new_snipet_path
+  fill_in "snipet_title", :with => "my first snippet"
+  fill_in "snipet_content", :with => "ls"
+  click_button "submit_snipet"
+
+  #FIXME: How can I get current_user id, in place of writing id ?
+  visit my_snipets_path(3)
+end
+
+When(/^I click edit on a snippet$/) do
+  click_link "edit"
+end
+
+When(/^I fill in the snippet edit form$/) do
+  fill_in "snipet_title", :with => "edited title"
+  fill_in "snipet_content", :with => "edited content"
+  click_button "Update Snipet"
+end
+
+Then(/^I should see my snippet page$/) do
+  expect(page).to have_content("Execution output")
+end
+
+When(/^I click delete on a snippet$/) do
+  click_link "delete"
+end
+
+When(/^I click ok on the confirm box$/) do
+  page.driver.browser.switch_to.alert.accept
+end
+
+Given(/^I am on the home page$/) do
+  visit snipets_path
+end
+
+When(/^I click show on a snippet$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see this snippet details$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see snippets page$/) do
+  expect(current_path).to match(/snipets/)
+end
+
+Then(/^I should see only my snippets$/) do
+  pending # express the regexp above with the code you wish you had
 end
