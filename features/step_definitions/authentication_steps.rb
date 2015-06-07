6017e78a5ca4e1b44238b179c6339b620ac4c754
Landry Soules <landry.soules@gmail.com>
2015-06-07 17:17:34 +0200

    Authentication feature.
    - new user account
    - sign in
    - sign out
    - incorrect sign up
    - incorrect login

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -19,3 +19,73 @@ Then(/^I should see "(.*?)"$/) do |message|
   expect(page).to have_content(message)
 end
 
+When(/^I go to signup page$/) do
+  visit "/users/sign_up"
+end
+
+When(/^I fill in registration form$/) do
+  @current_user = FactoryGirl.build(:new_user)
+  fill_in "user_username", with: @current_user.username
+  fill_in "user_email", with: @current_user.email
+  fill_in "user_password", with: @current_user.password
+  fill_in "user_password_confirmation", with: @current_user.password
+  click_button 'Sign up'
+end
+
+Then(/^I should see a success signup message$/) do
+  expect(page).to have_content 'Welcome! You have signed up successfully.'
+end
+
+Given(/^I am a registered user$/) do
+  @registered_user = FactoryGirl.create(:new_user)
+end
+
+When(/^I go to the signin page$/) do
+  visit new_user_session_path
+end
+
+When(/^I fill in the signin form$/) do
+  fill_in 'user_email', with: @registered_user.email
+  fill_in 'user_password', with: @registered_user.password
+  click_button 'Log in'
+end
+
+Then(/^I should see a success signin message$/) do
+  expect(page).to have_content 'Signed in successfully.'
+end
+
+Given(/^I am signed in$/) do
+  @already_signed_in = FactoryGirl.create(:new_user)
+end
+
+When(/^I click the signout link$/) do
+  visit destroy_user_session_path
+end
+
+Then(/^I should see a success signout message$/) do
+  expect(page).to have_content 'Signed out successfully.'
+end
+
+When(/^I fill in registration form with incorrect data$/) do
+  @current_user = FactoryGirl.build(:new_user)
+  fill_in "user_username", with: @current_user.username
+  fill_in "user_email", with: 'dummy_email'
+  fill_in "user_password", with: @current_user.password
+  fill_in "user_password_confirmation", with: @current_user.password
+  click_button 'Sign up'
+end
+
+Then(/^I should see a registration error$/) do
+  expect(page).to have_content 'prohibited this user from being saved'
+end
+
+When(/^I fill in the signin form with incorrect data$/) do
+  fill_in 'user_email', with: @registered_user.email
+  fill_in 'user_password', with: 'incorrectpassword'
+  click_button 'Log in'
+end
+
+Then(/^I should see a signin error$/) do
+  expect(page).to have_content 'Invalid email or password.'
+end
+
